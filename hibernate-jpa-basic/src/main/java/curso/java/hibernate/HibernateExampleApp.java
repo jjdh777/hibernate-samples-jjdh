2e9d0bea256cef64a34f8133f16008d4b61391fc
Jesus <jdaryanan2@alumno.uned.es>
2024-10-15 19:39:28 +0100

    20241015.1939 - Subida Ejercicio Terminado

--- hibernate-jpa-basic/src/main/java/curso/java/hibernate/HibernateExampleApp.java
+++ hibernate-jpa-basic/src/main/java/curso/java/hibernate/HibernateExampleApp.java
@@ -2,6 +2,7 @@ package curso.java.hibernate;
 
 import curso.java.hibernate.data.EmployeeRepository;
 import curso.java.hibernate.data.entity.Employee;
+import curso.java.hibernate.data.entity.Scope;
 import curso.java.hibernate.data.entity.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,8 +32,6 @@ public class HibernateExampleApp implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception
   {
-
-
     Employee emp2 = new Employee();
     emp2.setEmail("new Employee email");
     emp2.setFirstName("Bart");
@@ -44,22 +43,43 @@ public class HibernateExampleApp implements CommandLineRunner {
     Optional<Employee> emp = repository.findById(2L);
     emp.ifPresent(employee -> logger.info("Employee id 2 -> {}", emp.get()));
 
+    // Crear otro empleado
+    Employee emp3 = new Employee();
+    emp3.setEmail("new Employee email");
+    emp3.setFirstName("Capitan");
+    emp3.setLastName("America");
+    emp3.setTasks(getTasks());
+    repository.save(emp3);
+
+
     repository.findAll().forEach(System.out::println);
   }
 
   private Set<Task> getTasks() {
 
+    Scope scope1 = new Scope();
+    scope1.setName("NameScope 1");
+    scope1.setDescription("DescScope 1");
+    //scopeRepository.save(scope1);
+
+    Scope scope2 = new Scope();
+    scope2.setName("NameScope 2");
+    scope2.setDescription("DescScope 2");
+    //scopeRepository.save(scope2);
+
     Set<Task> tasks = new HashSet<>();
     Task task1 = new Task();
-    task1.setTaskName("report generation");
-    task1.setTaskDescription("Daily report generation");
+
+    task1.setTaskName("T1");
+    task1.setTaskDescription("Tarea 1");
+    task1.setScope(scope1);
     tasks.add(task1);
     Task task2 = new Task();
-    task2.setTaskName("view generation");
-    task2.setTaskDescription("Daily view generation");
+    task2.setTaskName("T2");
+    task2.setTaskDescription("Tarea 2");
+    task2.setScope((scope2));
     tasks.add(task2);
     return tasks;
   }
-
 
 }
